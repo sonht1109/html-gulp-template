61431272edc78ce438c21224d6dd094964231bb4
sonht <sonht1109@gmail.com>
2022-07-29 09:39:31 +0700

    first commit

---
+++ app/js/tool/bs-menu.js
@@ -0,0 +1,18 @@
+$(".show__menu").click(function () {
+    $(".menu").toggleClass("active");
+    $(".menu-top").removeClass("active")
+});
+$(".close__menu").click(function () {
+    $(".menu").removeClass("active")
+});
+$(".show__top").click(function () {
+    $(".menu-top").toggleClass("active");
+    $(".menu").removeClass("active")
+});
+$(".close__top").click(function () {
+    $(".menu-top").removeClass("active")
+});
+$("body").on("click", ".menu-list__item:has(.dropdown-list)", function(){
+    $(this).find(".dropdown-list").slideToggle();
+    $(this).siblings().find(".dropdown-list").slideUp();
+})
